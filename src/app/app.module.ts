6cef264668ab2b408abd68c3e9b36a068cb19383
Gonza <gonzabariloche@gmail.com>
2023-04-30 12:30:01 -0300

    intentando login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module'; // Agregamos esta línea
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,8 @@ import { ExperienciaEducacionComponent } from './componentes/experiencia-educaci
 import {HttpClientModule} from '@angular/common/http';
 import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +21,15 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     AcercaDeComponent,
     ExperienciaEducacionComponent,
     IniciarSesionComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAHOofU0wLUetw_L518gq6J0BqBCiUqIMA",
+    authDomain: "portfolio-dc0aa.firebaseapp.com",
+    projectId: "portfolio-dc0aa",
+    storageBucket: "portfolio-dc0aa.appspot.com",
+    messagingSenderId: "701689608352",
+    appId: "1:701689608352:web:fc276bd77cbfdcf1fe31c5",
+    measurementId: "G-MB4FGHSSCP"
+  };
